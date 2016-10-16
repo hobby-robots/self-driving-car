cbb0cc1e4c418cf430e3b87ee8e4e8d0b7234c26
Stas <stanislav.levental@gmail.com>
2016-10-15 17:29:02 -0700

    adding static pages

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"github.com/hobby-robots/self-driving-car/src/car"
 
+	"os"
+	"fmt"
 )
 
 func main() {
 	//steering := car.NewSteering(17, 27, 15, 18)
 	steering := car.DebugSteering()
-
 	defer steering.Close()
 
-	server := car.NewServer(8080, steering)
+	path := "/static"
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+
+	fmt.Printf("Serving static %s\n", os.Args)
+
+	server := car.NewServer(8080, steering, path)
 	if err := server.Start(); err != nil {
 		panic(err)
 	}
