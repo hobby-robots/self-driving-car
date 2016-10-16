994fc95afe969995c7be008e7eb0af7d73081216
Stas <stanislav.levental@gmail.com>
2016-10-15 18:07:49 -0700

    adding UI fix

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,46 +1,83 @@
 function sendReq(val) {
-  fetch('/' + val)
+    fetch('/' + val)
 }
 
 function left() {
-  sendReq('left');
-}
-
-function forward() {
-  sendReq('forward');
+    if (!turning) {
+        sendReq('left');
+        turning = true
+    }
 }
 
 function right() {
-  sendReq('right');
+    if (!turning) {
+        sendReq('right');
+        turning = true
+    }
+}
+
+function forward() {
+    if (!moving) {
+        sendReq('forward');
+        moving = true
+    }
 }
 
 function backward() {
-  sendReq('backward');
+    if (!moving){
+        sendReq('backward');
+        moving = true
+    }
 }
 
 function resetSteering() {
-  sendReq('resetSteering');
+    if (turning) sendReq('resetSteering');
+    turning = false;
 }
 
 function resetThrust() {
-  sendReq('resetThrust');
+    if (moving) sendReq('resetThrust');
+    moving = false;
 }
 
-var LEFT = 37
-var TOP = 38
-var RIGHT = 40
-var BOTTOM = 39
+var turning = false;
+var moving = false;
 
-document.body.onkeydown = function(e) {
-    console.warn();
+var LEFT = 37;
+var FORWARD = 38;
+var RIGHT = 39;
+var BACKWARD = 40;
+
+document.body.onkeydown = function (e) {
     switch (e.keyCode) {
-      case LEFT:
-        left()
-      case TOP:
-        forward()
-      case RIGHT:
-        right()
-      case BOTTOM:
-        backward()
+        case LEFT:
+            left()
+            break
+        case FORWARD:
+            forward()
+            break
+        case RIGHT:
+            right()
+            break
+        case BACKWARD:
+            backward()
+            break
+    }
+};
+
+document.body.onkeyup = function (e) {
+    switch (e.keyCode) {
+        case LEFT:
+            resetSteering()
+            break
+        case FORWARD:
+            resetThrust()
+            break
+        case RIGHT:
+            resetSteering()
+            break
+        case BACKWARD:
+            resetThrust()
+            break
     }
 };
