057c83f703b7f9240430bdbc29e10f8e8b5323cc
Sergii Rudenko <sergey.rudenko@grammarly.com>
2016-10-15 17:43:51 -0700

    add keydown handler

--- src/ui/index.js
+++ src/ui/index.js
@@ -25,3 +25,22 @@ function resetSteering() {
 function resetThrust() {
   sendReq('resetThrust');
 }
+
+var LEFT = 37
+var TOP = 38
+var RIGHT = 40
+var BOTTOM = 39
+
+document.body.onkeydown = function(e) {
+    console.warn();
+    switch (e.keyCode) {
+      case LEFT:
+        left()
+      case TOP:
+        forward()
+      case RIGHT:
+        right()
+      case BOTTOM:
+        backward()
+    }
+};
